bf42c301343a6f7b2c12501221b16c6c2cc93671
Snak <s.nakajima@4nonome.com>
2021-03-09 11:34:27 +0900

    Making page(0.0.3)

--- pjt00/tclone/urls.py
+++ pjt00/tclone/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('entry/', views.entry, name='entry')
+    path('', views.top, name='top'),
+    path('entry/', views.entry, name='entry'),
+    path('entry/confirm', views.entryconfirm, name='entryconfirm'),
+    path('entry/ok', views.ok, name='entryok'),
 ]
--- pjt00/tclone/views.py
+++ pjt00/tclone/views.py
@@ -1,12 +1,29 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import FormView, TemplateView
 
 # Create your views here.
+class CreateUser(FormView):
+    form_class = UserCreationForm
+    template_name = 'tclone/entry.html'
+    success_url = reverse_lazy('tclone:ok')
+    def form_valid(self, form):
+        status = self.request.post['judge']
+        if status == 'judge':
+            return render(request, 'tclone/ok.html')
 
+def top(request):
+    return render(request, 'tclone/top.html')
 
 def entry(request):
     return render(request, 'tclone/entry.html')
+
+def entryconfirm(request):
+    return render(request, 'tclone/entryconf.html')
+
+def ok(request):
+    return render(request, 'tclone/ok.html')
