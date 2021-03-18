457c31d47185b8686e13d472e225c0907eeb54c2
Snak <s.nakajima@4nonome.com>
2021-03-18 09:55:35 +0900

    Add tweet-model(3.0.2)

--- pjt00/tclone/models.py
+++ pjt00/tclone/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Tweet(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+    tweet = models.CharField(max_length=140)
+    def __str__(self):
+        return self.tweet
